002803ac827d38a27a3ee5beb8e3dcd2b1a6f30b
Jose Climaco <joseclimaco@me.com>
2016-12-07 16:41:02 +0000

    cenas

--- checklogin.php
+++ checklogin.php
@@ -2,21 +2,10 @@
 
     include_once "connection.php";
 
-<<<<<<< HEAD
-
-	mysqli_select_db(GetMyConnection(),'trialfct')
-		or die('Nao foi possi�vel seleccionar a base de dados');
-	
-	$sql = 'SELECT * FROM USERS WHERE (user = "'.$_POST["username"].'" AND pass = "'.$_POST["password"].'")';
-
-	$result = mysqli_query($g_link, $sql) //ver funcao "extract"
-		or die('Login Invalido!' . mysqli_error($g_link));
-=======
 	$sql = 'SELECT * FROM USERS WHERE (user = "'.$_POST["username"].'" AND pass = "'.$_POST["password"].'")';
 
 	$result = mysqli_query($g_link, $sql) //ver funcao "extract"
 		or die('Login Invalido!' . mysqli_error());
->>>>>>> github/master
 
 	$number = mysqli_num_rows($result); //se retornar 1, significa que � um utilizador valido
 	
